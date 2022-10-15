1a05944eabdd7908835ff9d58256db5e155bf332
admirableyetti <ech.emmanuel.ch@gmail.com>
2022-10-15 10:56:30 -0500

    feat(ui): job component

--- src/components/MainC/HomeExperience.vue
+++ src/components/MainC/HomeExperience.vue
@@ -2,19 +2,18 @@
 	<section :class="$style.experience">
 		<HomeTitle :class="$style.title" msg="Experience" />
 		<div :class="$style.jobs">
-			<div :class="$style.job">
-				<div :class="$style.company">
-					<a href="https://www.g4a.mx/" target="blank">
-						<h4>G4A</h4>
+			<div v-for="job in store.jobs" :key="job.id" :class="$style.company">
+				<div>
+					<a :href="job.url" target="blank">
+						<h4>{{ job.name }}</h4>
 					</a>
-					<span>Feb 2021 - Present</span>
+					<span>{{ job.date }}</span>
 				</div>
 				<p :class="[store.state.isActive ? 'textLight' : 'textDark']">
-					Front-end developer
+					{{ job.role }}
 				</p>
 			</div>
-			<a :class="$style.resume" href="CV_English.docx.pdf" target="blank"
-				>View My Resume👉
+			<a :class="$style.resume" href="CV_English.docx.pdf" target="blank">View My Resume👉
 			</a>
 		</div>
 	</section>
@@ -24,15 +23,13 @@
 import HomeTitle from "./HomeTitle.vue";
 import { inject } from "@vue/runtime-core";
 
-// import "";
-
 export default {
 	components: {
 		HomeTitle,
 	},
 	setup() {
 		const store = inject("store");
-
+		console.log(store);
 		return {
 			store,
 		};
@@ -44,42 +41,49 @@ export default {
 .experience {
 	display: flex;
 }
+
 @media screen and (max-width: 768px) {
 	.experience {
 		flex-direction: column;
 	}
 }
+
 .experience p {
 	margin: 0;
 }
+
 .jobs {
 	width: 100%;
 	word-break: break-all;
+	margin-bottom: 100px;
 }
+
 @media screen and (max-width: 768px) {
 	.jobs {
 		padding-left: 50px;
 	}
 }
-.job {
-	margin-bottom: 100px;
-}
+
 .company {
 	display: flex;
 	justify-content: space-between;
-	margin-bottom: 10px;
+	margin-bottom: 30px;
 }
+
 .company a {
 	text-decoration: none;
 	color: inherit;
 }
+
 .company h4 {
 	margin: 0;
 	font-weight: 900;
 }
+
 .company span {
 	font-weight: 300;
 }
+
 .resume {
 	font-size: 18px;
 	font-weight: 700;
